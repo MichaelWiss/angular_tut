7b82048bfabb39d990627971b5d709518c8226b8
Michael Wiss <michael.wiss@gmail.com>
2015-08-09 17:53:39 -0500

    added bootstrap, added links, added upvoting

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
 angular.module('flapperNews', [])
-.controller('MainCtrl', [
-'$scope',
-function($scope){
-  
-  $scope.posts = [
+.controller('MainCtrl', ['$scope', function($scope){
+	$scope.posts = [
     {title: 'post 1', upvotes: 5},
     {title: 'post 2', upvotes: 2},
     {title: 'post 3', upvotes: 15},
@@ -11,7 +8,16 @@ function($scope){
     {title: 'post 5', upvotes: 4}
   ];
    $scope.addPost = function(){
-   $scope.posts.push({title: $scope.title, upvotes: 0});
-   $scope.title = '';
- };
-}]);
+       if(!$scope.title || $scope.title === '') { return; } 
+       $scope.posts.push({
+     	     title: $scope.title,
+     	     link: $scope.link, 
+     	     upvotes: 0
+     });
+     $scope.title = '';
+     $scope.link = '';
+   };
+   $scope.incrementUpvotes = function(post) {
+		post.upvotes += 1;
+	};
+ }]);
